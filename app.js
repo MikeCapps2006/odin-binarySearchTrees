f7db4ed757f96daa7d82b2942da53050a26d4ec4
Mike Capps <michaelcapps1772@yahoo.com>
2023-08-26 08:30:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+import Tree from './tree.js';
+
+const tree = new Tree([1, 2, 3, 4, 5]);
+
---
+++ tree.js
@@ -0,0 +1,21 @@
+import Node from './node.js';
+
+export default class Tree {
+    constructor (arr) {
+        this.root = arr[0];
+        this.data = arr;
+    }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
